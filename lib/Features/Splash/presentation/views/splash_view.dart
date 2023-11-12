717c55fe54bcb4060d8651d16a99c55aac069bbe
omarabdelaziz26-6 <omar47306@gmail.com>
2023-11-12 20:38:14 +0200

    refactor the code

--- lib/Features/Splash/presentation/views/splash_view.dart
+++ lib/Features/Splash/presentation/views/splash_view.dart
@@ -1,7 +1,6 @@
 import 'package:bookly/Features/Splash/presentation/views/widgets/splash_view_body.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+
 
 class SplashView extends StatelessWidget {
   const SplashView({Key? key}) : super(key: key);
--- lib/core/utils/functions/build_error_snack_bar.dart
+++ lib/core/utils/functions/build_error_snack_bar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../../../Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart';
 
 SnackBar buildErrorWidget(String errMessage) {
   return SnackBar(
